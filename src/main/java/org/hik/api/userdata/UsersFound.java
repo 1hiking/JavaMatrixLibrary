735983ceebf1576d9209b0416379610462aee35d
J.C <73438877+1hiking@users.noreply.github.com.>
2026-07-06 20:47:34 -0600

    Remove orphan comments

--- src/main/java/org/hik/api/userdata/UsersFound.java
+++ src/main/java/org/hik/api/userdata/UsersFound.java
@@ -3,14 +3,11 @@ package org.hik.api.userdata;
 import java.net.URI;
 import java.util.List;
 
-/// @param limited
-/// @param results
+
 public record UsersFound(Boolean limited,
                          List<User> results) {
 
-    /// @param avatarUrl
-    /// @param displayName
-    /// @param userId
+
     public record User(URI avatarUrl,
                        String displayName,
                        String userId) {
