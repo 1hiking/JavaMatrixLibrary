32e7a482f49130827888b5a2ffa0b9ab423985c9
J.C <73438877+1hiking@users.noreply.github.com.>
2026-07-25 14:36:41 -0600

    Patch the API Client

--- src/test/java/org/hik/api/MatrixAPIClientTest.java
+++ src/test/java/org/hik/api/MatrixAPIClientTest.java
@@ -1,50 +1,41 @@
 package org.hik.api;
 
 
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
-import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import org.hik.context.DiscoveryResponse;
+import org.instancio.junit.Given;
+import org.instancio.junit.InstancioExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@SuppressWarnings("java:S5786")
-public class MatrixAPIClientTest {
-    private static final String USER = "test";
+@ExtendWith(InstancioExtension.class)
+@WireMockTest
+class MatrixAPIClientTest {
     private static final String AUTH_TOKEN = "1234";
 
 
-    @RegisterExtension
-    protected static final WireMockExtension wireMockServer = WireMockExtension.newInstance()
-            .options(WireMockConfiguration.wireMockConfig()
-                    .dynamicPort()
-                    .usingFilesUnderClasspath("wiremock"))
-            .build();
-
     @BeforeEach
-    void setUp() {
-        wireMockServer.stubFor(get(urlEqualTo("/.well-known/matrix/client"))
+    void setUp(WireMockRuntimeInfo wireMockRuntimeInfo) {
+        stubFor(get(urlEqualTo("/.well-known/matrix/client"))
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withHeader("Content-Type", "application/json")
-                        .withBody("{\"m.homeserver\": {\"base_url\": \"" + wireMockServer.baseUrl() + "\"}}")));
+                        .withBody("{\"m.homeserver\": {\"base_url\": \"" + wireMockRuntimeInfo.getHttpBaseUrl() + "\"}}")));
 
     }
 
+    @Given
+    private DiscoveryResponse discoveryResponse;
 
     @Test
-    void getWellKnown_WithAllRequiredProperties_thenReturnCorrectSerialization() {
-        var client = MatrixClient.create(wireMockServer.baseUrl(), USER, AUTH_TOKEN);
+    void getWellKnown_WithAllRequiredProperties_thenReturnCorrectSerialization(WireMockRuntimeInfo wireMockRuntimeInfo) {
+        var client = MatrixClient.create(discoveryResponse, AUTH_TOKEN);
         assertDoesNotThrow(() -> client, "The client should not throw given a good url.");
-    }
-
-    @Test
-    void getWellKnown_WithBadUrl_thenReturnAnException() {
-        assertThrows(IllegalArgumentException.class, () -> MatrixClient.create("INCORRECT.ORG", USER, AUTH_TOKEN),
-                "The client should throw when given a bad url.");
     }
 
 
